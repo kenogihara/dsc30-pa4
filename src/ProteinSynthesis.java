6e6665835d373b5fd8fd28d1e6b62e9d671a959d
kenogihara <kenogihara808@gmail.com>
2024-02-08 13:36:14 -0800

    bruh

--- src/ProteinSynthesis.java
+++ src/ProteinSynthesis.java
@@ -50,27 +50,21 @@ class ProteinSynthesis {
         if (rna.isEmpty()) {
             return emptyQueue;
         }
-        if (rna.peek() == 'A' && rna.myList.get(1) == 'U' && rna.myList.get(2) == 'G') {
-            String codon = "";
-            MyQueue<String> aminoAcidChain = new MyQueue<>(rna.size() / CODON);
-            int groups = rna.size() / CODON;
-            for (int i = 0; i < groups; i++) {
-                codon += rna.dequeue() + "" + rna.dequeue() + "" + rna.dequeue();
-                if (codon.equals("UAA") || codon.equals("UAG") || codon.equals("UGA")) {
-                    return aminoAcidChain;
-                } else {
-                    aminoAcidChain.enqueue(CodonMap.getAminoAcid(codon));
-                }
-                codon = "";
+        String codon = "";
+        boolean startTranscription;
+        MyQueue<Character> aminoAcidChain = new MyQueue<>;
+        int groups = rna.size() / CODON;
+        for (int i = 0; i < groups; i++) {
+            codon += rna.dequeue() + rna.dequeue() + rna.dequeue();
+            if (codon.equals("UAA") || codon.equals("UAG") || codon.equals("UGA")) {
+                break;
             }
-            return aminoAcidChain;
-
-        } else {
-            rna.dequeue();
-            // we remove 3 elements because we count the dna sequence in 3's.
-            rna.dequeue();
-            rna.dequeue();
-            return translateRNA(rna);
-        }
+            if (codon.equals("AUG")) {
+                startTranscription = true;
+            }
+            if (startTranscription) {
+                aminoAcidChain.enqueue(CodonMap.getAminoAcid(codon));
+            }
+        } return aminoAcidChain;
     }
 }
