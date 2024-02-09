24317a37d1d1bb34b05cb7e8c51864981049ce04
kenogihara <kenogihara808@gmail.com>
2024-02-08 22:28:36 -0800

    bruh

--- src/DoublyLinkedListTest.java
+++ src/DoublyLinkedListTest.java
@@ -96,7 +96,7 @@ class DoublyLinkedListTest {
         numbers.add(800);
         assertFalse(numbers.isEmpty());
         assertTrue(flights.isEmpty());
-        numbers.removeFirst();
+        numbers.remove(0);
         assertTrue(numbers.isEmpty());
     }
 
