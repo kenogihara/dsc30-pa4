f0b2bc3a307b3ce6149444240e09bbe97841d79a
kenogihara <kenogihara808@gmail.com>
2024-02-08 18:58:00 -0800

    bruh

--- src/ProteinSynthesisTest.java
+++ src/ProteinSynthesisTest.java
@@ -6,30 +6,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProteinSynthesisTest {
 
     ProteinSynthesis genetics;
-    MyQueue<Character> forTesting;
     @BeforeEach
     void setUp() {
         genetics = new ProteinSynthesis();
     }
     @Test
     void transcribeDNA() {
-        forTesting = genetics.transcribeDNA("AAGAGATGTCGGTTTGCTAGTTCACGTAAAGGT");
-        assertEquals("AAGAGAUGUCGGUUUGCUAGUUCACGUAAAGGU", forTesting.myList.toTestString());
+        assertEquals("AAGAGAUGUCGGUUUGCUAGUUCACGUAAAGGU", genetics.transcribeDNA("AAGAGATGTCGGTTTGCTAGTTCACGTAAAGGT").myList.toTestString());
 
         assertEquals("GCCUCGCGCCACCUCUAAGUAAGUGAGCCGUCGAGACAUUAUCCCUGAUUUUCUCACUACUAUUAGUACUCACGGCGCAAUACCACCACAGCCUUGUCU",
-                String.valueOf(genetics.transcribeDNA("GCCTCGCGCCACCTCTAAGTAAGTGAGCCGTCGAGACATTATCCCTGATTTTCTCACTACTATTAGTACTCACGGCGCAATACCACCACAGCCTTGTCT").myList));
+                genetics.transcribeDNA("GCCTCGCGCCACCTCTAAGTAAGTGAGCCGTCGAGACATTATCCCTGATTTTCTCACTACTATTAGTACTCACGGCGCAATACCACCACAGCCTTGTCT").myList.toTestString());
 
         assertThrows(IllegalArgumentException.class, () -> genetics.transcribeDNA("CGCCA"));
 
-        assertFalse(String.valueOf(genetics.transcribeDNA("GAATGCCGGTCAGCA").myList).contains("T"));
+        assertFalse(genetics.transcribeDNA("GAATGCCGGTCAGCA").myList.toTestString().contains("T"));
     }
 
     @Test
     void translateRNA() {
-        assertEquals("MYS", String.valueOf(genetics.translateRNA(genetics.transcribeDNA("ATGTATTCT")).myList));
-        assertEquals("MS", String.valueOf(genetics.translateRNA(genetics.transcribeDNA("AGGATGTCT")).myList));
-        assertEquals("M\u0000\u0000", String.valueOf(genetics.translateRNA(genetics.transcribeDNA("ACAGAGATGTAATAC")).myList));
-        assertEquals("MELPY", String.valueOf(genetics.translateRNA(genetics.transcribeDNA("ATGGAGTTGCCGTAC")).myList));
+        assertEquals("MYS", genetics.translateRNA(genetics.transcribeDNA("ATGTATTCT")).myList.toTestString());
+        assertEquals("MS", genetics.translateRNA(genetics.transcribeDNA("AGGATGTCT")).myList.toTestString());
+        assertEquals("M**", genetics.translateRNA(genetics.transcribeDNA("ACAGAGATGTAATAC")).myList.toTestString());
+        assertEquals("MELPY", genetics.translateRNA(genetics.transcribeDNA("ATGGAGTTGCCGTAC")).myList.toTestString());
         assertTrue(genetics.translateRNA(genetics.transcribeDNA("GAATGCCGGTCAGCA")).isEmpty());
     }
 }
